5208ce89144e8e5078be20321644e2505b87a825
mizanurmillon <mr7517218@gmail.com>
2026-04-23 16:58:21 +0600

    sat the ftp

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -27,6 +27,9 @@ return Application::configure(basePath: dirname(__DIR__))
             Route::middleware(['web','auth','admin'])
                 ->prefix('admin')
                 ->group(base_path('routes/admin_setting.php'));
+                
+            Route::middleware(['web'])
+                ->group(base_path('routes/frontend.php'));
         }
     )
     ->withMiddleware(function (Middleware $middleware): void {
